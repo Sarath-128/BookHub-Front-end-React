45911063308c566a771c77023f5d4c0685f48fea
Sarath-128 <pavisarath2002@gmail.com>
2024-09-28 12:20:59 +0530

    BookHub

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+
+import UserDetail from './Components/UserDetail';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import React from 'react';
+import CreateUser from './Components/CreateUser';
+import Scrollspy from './Components/Scrollspy';
+import BookDetails from './Components/BookDetails';
+import CreateBook from './Components/CreateBook';
+import Trial from'./Components/Trial'
+import Register from './Components/Credentials/Register';
+import Login from './Components/Credentials/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UD from "./Components/UD"
+import BD from "./Components/BD"
+
+
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+{/* <CreateBook/> */}
+{/* <BookDetails/> */}
+{/* <Scrollspy/> */}
+{/* <UserDetail/> */}
+{/* <CreateUser/> */}
+<ToastContainer />
+{/* <Trial/> */}
+{/* <Register/> */}
+{/* <Login/> */}
+
+
+
+<Router>
+  <Routes>
+    <Route path='/' element={<Login/>}></Route>
+    <Route path='register/' element={<Register/>}></Route>
+    <Route path='dashboard/' element={<Scrollspy/>}></Route>
+    <Route path='userdetails/' element={<UD/>}></Route>
+    <Route path='bookdetails/' element={<BD/>}></Route>
+  </Routes>
+</Router>
+
+
+
+</>
   );
 }
 
